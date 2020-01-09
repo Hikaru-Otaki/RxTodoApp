c3332271d188d02c60fae1eae52781c0b173e803
Hikaru <hikaru.11.oth@icloud.com>
2020-01-10 00:07:26 +0900

    Initial Commit

---
+++ RxTodoAppTests/RxTodoAppTests.swift
@@ -0,0 +1,34 @@
+//
+//  RxTodoAppTests.swift
+//  RxTodoAppTests
+//
+//  Created by 大瀧輝 on 2020/01/10.
+//  Copyright © 2020 Hikaru Otaki. All rights reserved.
+//
+
+import XCTest
+@testable import RxTodoApp
+
+class RxTodoAppTests: XCTestCase {
+
+    override func setUp() {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
